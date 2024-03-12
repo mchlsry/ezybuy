771193d3e6fa8e4b44708c069850bb14815e740d
mchlsry <136965869+mchlsry@users.noreply.github.com>
2024-03-12 13:31:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// slider
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter>3){
+        counter = 1
+    }
+}, 5000); // -> 5 detik
